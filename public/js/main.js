4c7a260c4bc4c081b24c3d6aba1bb91f93ac6aee
Wagner Leonardi <leonardiwagner@gmail.com>
2015-02-09 16:34:12 -0200

    #2 angular improvments

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,65 @@
 var angularApp = angular.module('angularApp', []);
 
 angularApp.controller('Ctrl', function ($scope, $http) {
-  $scope.app = {input: "oi", key: "teste", output: "afer"};
+	$scope.app = {
+		inputLabel: "",
+		outputLabel: "",
+		input: "",
+		output: "",
+		key: "",
+		successMessage: "",
+		commandName: "",
+		commandFunction: null,
+		hasCommandExecuted: false
+	};
 
-  $scope.encrypt = function(){
-    $http.post('/encrypt', {text: $scope.app.input, key: $scope.app.key})
-    .success(function(data, status, headers, config) {
-      // this callback will be called asynchronously
-      // when the response is available
-      
-      $scope.app.output = data;
+	var encrypt = function(isEncrypt){
+		var url = mode ? "/decrypt" : "/decrypt";
+		$http.post(url, {text: $scope.app.input, key: $scope.app.key})
+		.success(function(data, status, headers, config) {
+			$scope.app.output = data;
+			$scope.app.isCommandExecuted = true;
+		})
+		.error(function(data, status, headers, config) {
+		});
+	};
+	
+  var mode = [
+	{
+		inputLabel: "Text to encrypt",
+		outputLabel: "Encrypted text",
+		commandName: "Encrypt Text",
+		commandFunction: encrypt(true),
+		successMessage: "Text encrypted! Now you can store this encrypted text anywhere and no one will know what it means without the correct key! Store the key somewhere safe, or share with people that you want to decrypt the message",
+		subCommand1Name: "New Encrypt",
+		subCommand2Name: "Decrypt Text"
+	},{
+		inputLabel: "Text to decrypt",
+		outputLabel: "Decrypted text",
+		commandName: "Decrypt Text",
+		commandFunction: encrypt(false),
+		successMessage: "Text decrypted! If you used the correct key probably you can read now. If you can't read, you've used a wrong key, or this is not your business!",
+		subCommand1Name: "New Decrypt",
+		subCommand2Name: "Encrypt Text"
+	}
+  ];
 
-    })
-    .error(function(data, status, headers, config) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-    });
-  };
+	$scope.changeMode = function(mode){
+		setMode(mode);
+	};
+	
+	var setMode = function(modeIndex){
+		$scope.app.inputLabel = mode[modeIndex].inputLabel;
+		$scope.app.outputLabel = mode[modeIndex].outputLabel;
+		$scope.app.commandName = mode[modeIndex].commandName;
+		$scope.app.successMessage = mode[modeIndex].successMessage;
+		$scope.app.subCommand1Name = mode[modeIndex].subCommand1Name;
+		$scope.app.subCommand2Name = mode[modeIndex].subCommand2Name;
+	};
+	
+	setMode(0);
+	
+	
 
 
 
